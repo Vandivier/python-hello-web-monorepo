4851320c975cec33388d0c8fbe74406838516271
John Vandivier <vandivi3r@gmail.com>
2023-02-27 00:41:04 -0500

    feat: codeploy flask
    and django

--- third_party_auth_api/app.py
+++ third_party_auth_api/app.py
@@ -14,6 +14,15 @@ def home():
 
     return create_json_response_for_permissions(False)
 
+@app.route('/third-party-auth', methods=['GET'])
+def vercel_compatible_home():
+    username = request.args.get('username', default = "", type = str)
+
+    if username:
+        return create_json_response_for_permissions("admin" in username.lower())
+
+    return create_json_response_for_permissions(False)
+
 @app.route('/about')
 def about():
     return 'About'
