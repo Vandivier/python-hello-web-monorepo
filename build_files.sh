1be054216607f2821da242d0d6597d389fdbe671
John Vandivier <vandivi3r@gmail.com>
2023-03-19 22:08:32 -0400

    fix: admin UI

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 # build_files.sh
+# ref: https://www.devmaesters.com/blog/15
 pip install -r requirements.txt
-# python3.9 manage.py collectstatic
 python manage.py collectstatic --noinput
--- django_poetry_example/settings.py
+++ django_poetry_example/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Quick-start development settings - unsuitable for production
