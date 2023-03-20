ef418c367d1195fc81f6fd198b4e7d7269058365
John Vandivier <vandivi3r@gmail.com>
2023-03-19 22:24:05 -0400

    fix: admin link

--- django_poetry_example/urls.py
+++ django_poetry_example/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin', RedirectView.as_view(url='admin/login', permanent=False)),
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
